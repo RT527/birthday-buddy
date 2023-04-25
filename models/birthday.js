de9fbeaa7662b2cefc124807fca83ec4415ff7d1
RT527 <rafi.talukder7@gmail.com>
2023-04-25 09:09:26 -0400

    model update

--- models/birthday.js
+++ models/birthday.js
@@ -1,3 +1,20 @@
 import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  content: String,
+  rating: { type: Number, min: 1, max: 5, default: 5 }
+}, {
+  timestamps: true
+})
+
+const birthday = new Schema({
+  
+})
+
+const birthday = mongoose.model('birthday', birthdaySchema)
+
+export {
+  Birthday
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // import npm packages
+import "dotenv/config.js"
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -39,10 +40,11 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
   // render the error page
   res.status(err.status || 500)
   res.render('error')
 })
+
+
 
 export { app }
