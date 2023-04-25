11a01827ead24e7da7edfde3e9750f0d50ddd1a6
RT527 <rafi.talukder7@gmail.com>
2023-04-25 16:16:54 -0400

    mods add

--- models/birthday.js
+++ models/birthday.js
@@ -2,39 +2,16 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
-const birthdayShema = new Schema ({
-  birthday: {
-    type: Date,
-    default: function(){
-      let newDate = new Date()
-      newDate.setFullYear(newDate.getFullYear()+1)
-      return newDate
-    }
-  },
-  flightNo: {
-    type: Number,
-    required: true,
-    min: 10,
-    max: 9999,
+const giftSchema = new Schema ({
+  name: String,
+  description: String
+})
 
-  },
-  airline: {
-    type: String,
-    enum: ['American', 'Southwest', 'United'],
-    required: true,
-  },
-  departs: {
-    type: Date,
-    default: function(){
-      let newDate = new Date()
-      newDate.setFullYear(newDate.getFullYear()+1)
-      return newDate
-    }
-  },
-  tickets: [ticketSchema],
-  meals:[{type: Schema.Types.ObjectId, ref:'Meal'}]
-}, {
-  timestamps: true,
+const birthdaySchema = new Schema ({
+  birthday : {type: Date, required: true },
+  personName : {type: String, required: true},
+  notes: String,
+  gifts: [giftSchema]
 })
 
 const Birthday = mongoose.model('Birthday', birthdaySchema)
